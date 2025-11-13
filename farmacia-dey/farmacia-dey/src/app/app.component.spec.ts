d02842be5b33ae645e64e4972ce2670fac6b9f14
Pablo Huerta <pabloh1545@gmail.com>
2025-11-12 20:21:29 -0500

    fix

--- farmacia-dey/farmacia-dey/src/app/app.component.spec.ts
+++ farmacia-dey/farmacia-dey/src/app/app.component.spec.ts
@@ -1,29 +1,31 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { MatDialogRef } from '@angular/material/dialog';
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from '../../../src/app/app.component';
 
 describe('AppComponent', () => {
-  let component: AppComponent;
-  let fixture: ComponentFixture<AppComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientModule],
-      declarations: [AppComponent],
-      providers: [
-        { provide: MatDialogRef, useValue: {} }
-      ]
+      imports: [RouterTestingModule],
+      declarations: [AppComponent]
     }).compileComponents();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AppComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 
-  it('should create the app', () => {
-    expect(component).toBeTruthy();
+  it(`should have as title 'farmacia-dey'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('farmacia-dey');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('farmacia-dey');
   });
 });
