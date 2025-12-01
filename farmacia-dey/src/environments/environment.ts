cc3ab1d154454ee5fa5e263fbaaae2621c624cf1
Pablo Huerta <pabloh1545@gmail.com>
2025-12-01 17:33:49 -0500

    fix_environment

--- farmacia-dey/src/environments/environment.ts
+++ farmacia-dey/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   version: '1.0.0',
-  baseUrlBackend: 'http://localhost:9000',
+  baseUrlBackend: 'https://farmacia-gateway-kdkd.onrender.com',
 
   security: {
     secretKey: 'U2FsdGVkX18f0Vl+x9f5Hm1s2TEBv6VKVy2AxiXf3PgK2xpcOMtOtPe6yFyfYYbH'
